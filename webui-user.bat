4966feecba4feac36ab873a776b53172d1460205
flufy3d <flufy3d@gmail.com>
2023-01-14 15:47:38 +0100

    start from conda and add some start args

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,12 @@
 @echo off
 
+call C:\Users\flufy3d\anaconda3\Scripts\activate.bat sd-webui
+
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--xformers --listen --enable-insecure-extension-access
 
 call webui.bat
+
+
